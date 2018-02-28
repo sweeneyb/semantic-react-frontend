58f591a9e667e96e5e25550f7c8d2d795f4dfcbb
sweeneyb <tab126@gmail.com>
2018-02-28 05:50:26 +0000

    search with semantic-ui-react sorta works

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
+import _ from 'lodash'
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.svg'; 
 import './App.css';
 import axios from 'axios';
+import { Search, Grid, Header } from 'semantic-ui-react'
+
 
 class App extends Component {
-
-    state = { name: ""}
-
+    componentWillMount() {
+	this.resetComponent()
+    }
+    
     componentDidMount() {
-	axios.get(App.url)
-	    .then( res => this.setState(res.data) )
+	axios.get('/restaurants')
+	    .then( res => this.setState( {restaurants: res.data._embedded}) )
 	    .catch(function (error) {
 		console.log(error);
 	    });
     }
+
+
+    resetComponent = () => this.setState({ isLoading: false, results: [], value: '' })
+
+    handleResultSelect = (e, { result }) => this.setState({ value: result.name })
+
+    handleSearchChange = (e, { value }) => {
+	this.setState({ isLoading: true, value })
+
+	setTimeout(() => {
+	    if (this.state.value.length < 1) return this.resetComponent()
+
+	    const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+	    const isMatch = result => re.test(result.name)
+
+	    this.setState({
+		isLoading: false,
+		results: _.filter(this.state.restaurants.restaurants, isMatch),
+	    })
+	}, 500)
+    }
+
     
-  render() {
+    
+    render() {
+	const {isLoading, value, results } = this.state
     return (
       <div className="App">
         <header className="App-header">
@@ -24,7 +52,26 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+            </p>
+
+	<Grid>
+            <Grid.Column width={8}>
+          <Search
+        loading={isLoading}
+        onResultSelect={this.handleResultSelect}
+        onSearchChange={this.handleSearchChange}
+        results={results}
+        value={value}
+        {...this.props}
+          />
+        </Grid.Column>
+            <Grid.Column width={8}>
+          <Header>State</Header>
+            <pre>{JSON.stringify(this.state, null, 2)}</pre>
+          <Header>Options</Header>
+            <pre>{JSON.stringify(this.state.restaruants, null, 2)}</pre>
+        </Grid.Column>
+      </Grid>
       </div>
     );
   }
